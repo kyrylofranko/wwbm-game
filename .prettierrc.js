bc24ea705b74c5b9a24e93604a5297b9ad441c15
Kira Marks <60671560+kiramarks@users.noreply.github.com>
2020-10-23 11:58:06 +0300

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports =  {
+  parser: "typescript",
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+};
