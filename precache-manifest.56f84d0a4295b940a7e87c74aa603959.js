b3f599915d09ad8ab2e8741a40847205259ff3b8
Kira Marks <kiramarks.x@gmail.com>
2020-10-26 18:31:08 +0200

    Updates

---
+++ precache-manifest.56f84d0a4295b940a7e87c74aa603959.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d397b71f70430e1c525a4a51a3e9150",
+    "url": "/wwbm-game/index.html"
+  },
+  {
+    "revision": "c044f9a5e5f9b910b8bd",
+    "url": "/wwbm-game/static/css/main.e70b273c.chunk.css"
+  },
+  {
+    "revision": "2db88ff65b91ca210731",
+    "url": "/wwbm-game/static/js/2.036b3a41.chunk.js"
+  },
+  {
+    "revision": "3cb9a959562221565f2e9d8bda1289d3",
+    "url": "/wwbm-game/static/js/2.036b3a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca072cdd87b61411570d",
+    "url": "/wwbm-game/static/js/3.ebba2cf4.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/wwbm-game/static/js/3.ebba2cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c044f9a5e5f9b910b8bd",
+    "url": "/wwbm-game/static/js/main.96ab59c3.chunk.js"
+  },
+  {
+    "revision": "8ddf9081a38948bb5f27",
+    "url": "/wwbm-game/static/js/runtime-main.4a73cf99.js"
+  },
+  {
+    "revision": "d27f45adf88f4e70e787423e4db695ad",
+    "url": "/wwbm-game/static/media/correct_answer.d27f45ad.mp3"
+  },
+  {
+    "revision": "d9cb525d71e4df9dc610ff32e85a3276",
+    "url": "/wwbm-game/static/media/easy.d9cb525d.mp3"
+  },
+  {
+    "revision": "b00923136c9d4630ec067bc1260136a2",
+    "url": "/wwbm-game/static/media/final_answer.b0092313.mp3"
+  },
+  {
+    "revision": "1f1e4da13a93018a5b848c4cf3489057",
+    "url": "/wwbm-game/static/media/hand.1f1e4da1.png"
+  },
+  {
+    "revision": "15496a46b4daa01b37f882ce98a38d73",
+    "url": "/wwbm-game/static/media/hard.15496a46.mp3"
+  },
+  {
+    "revision": "eeb77fd73e82801e7fd53dc9d31285b8",
+    "url": "/wwbm-game/static/media/hard_million.eeb77fd7.mp3"
+  },
+  {
+    "revision": "3d8ba0a6804c784263ebab21dc1f4027",
+    "url": "/wwbm-game/static/media/lets_play.3d8ba0a6.mp3"
+  },
+  {
+    "revision": "b3f5ba3c07400794a79c08f0443ca7b8",
+    "url": "/wwbm-game/static/media/main_theme.b3f5ba3c.mp3"
+  },
+  {
+    "revision": "78fa537627a5da1e08b44e21eaea4165",
+    "url": "/wwbm-game/static/media/medium.78fa5376.mp3"
+  },
+  {
+    "revision": "096233cfac18fedc6306cfb1e7fe57dd",
+    "url": "/wwbm-game/static/media/next.096233cf.mp3"
+  },
+  {
+    "revision": "b1412542e669dfed09b312c5b601957c",
+    "url": "/wwbm-game/static/media/winning_theme.b1412542.mp3"
+  },
+  {
+    "revision": "e55862ad3acb9ac8ac4454221366f389",
+    "url": "/wwbm-game/static/media/wrong_answer.e55862ad.mp3"
+  }
+]);
